ce6c4a7372f1d85e9822796385de350e270e62f3
Austin Lynn <auslynn@iu.edu>
2020-10-21 20:43:24 -0400

    created second scene, a lot of prefabs, some platforms, and a scene changer script

---
+++ Midterm Project/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    Collider2D col;
+    //Collider2D playerCollider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        col = GetComponent<Collider2D>();
+        //playerCollider = player.GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "Player" && this.name == "SceneChanger1")
+        {
+            SceneManager.LoadScene("Scene2");
+        }
+    }
+}
