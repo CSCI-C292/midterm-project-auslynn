9d3dd75cced746319f55ce75e8c6bb74b20ff988
Austin Lynn <auslynn@iu.edu>
2020-11-17 04:51:35 -0500

    bat movement implemented (same as snake for now, will likely add vertical movement later).

---
+++ Midterm Project/Assets/Scripts/BatController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatController : MonoBehaviour
+{
+    public int moveSpeed;
+    public float switchInterval;
+    public SpriteRenderer spriteRenderer;
+    private float tempTime;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        tempTime = switchInterval;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(switchInterval > tempTime / 2)
+        {
+            rb.velocity = new Vector2(moveSpeed, 0);
+            switchInterval -= Time.deltaTime;
+        }
+        else if (switchInterval > 0)
+        {
+            rb.velocity = new Vector2(-moveSpeed, 0);
+            switchInterval -= Time.deltaTime;
+        }
+        else
+        {
+            switchInterval = tempTime;
+        }
+
+        if(rb.velocity.x >= 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else
+        {
+            spriteRenderer.flipX = true;
+        }
+    }
+}
